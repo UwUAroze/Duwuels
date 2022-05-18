4f48f94bf70702f3cc784647917e7d683c0c93dd
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-18 22:07:50 +0100

    aa

---
+++ src/main/java/me/aroze/duwuels/Duwuels.java
@@ -0,0 +1,17 @@
+package me.aroze.duwuels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Duwuels extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
