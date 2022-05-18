e6e72ae48661f13eefe465d2f9da7120796495ab
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-18 23:47:47 +0100

    Changed syntax (for the last time) and added another message for specifying an offline player

--- src/main/java/me/aroze/duwuels/commands/DuelCommand.java
+++ src/main/java/me/aroze/duwuels/commands/DuelCommand.java
@@ -1,6 +1,7 @@
 package me.aroze.duwuels.commands;
 
 import me.aroze.duwuels.util.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +12,15 @@ public class DuelCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (args.length > 1) {
-            sender.sendMessage(ChatUtils.color("\n &e&l⚠ &eDuels &6» &eSyntax:\n  &f/duel &7[Public Queues] \n  &f/duel <player> &7[Private Duel]\n "));
+            sender.sendMessage(ChatUtils.color("\n &6⚠ &eDuels &7» &eSyntax:\n  &f/duel &7[Public Duel Queues] \n  &f/duel <player> &7[Private Duel]\n "));
             return true;
+        }
+
+        if (args.length == 1) {
+            if (!(Bukkit.getPlayer(args[0]).isOnline())) {
+                sender.sendMessage(ChatUtils.color("\n &6⚠ &eDuels &7» &eSyntax:\n  &f/duel &7[Public Duel Queues] \n  &f/duel <player> &7[Private Duel]\n "));
+                sender.sendMessage(ChatUtils.color("\n &7You seem to have specified a player: &e " + args[0] + "\n  ,&7but they are not online ;c"));
+            }
         }
 
         return true;
