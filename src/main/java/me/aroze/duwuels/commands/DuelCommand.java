d43a48d6a52a8e06fb8b2acdad4af6747831f07b
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 00:36:57 +0100

    a

--- src/main/java/me/aroze/duwuels/commands/DuelCommand.java
+++ src/main/java/me/aroze/duwuels/commands/DuelCommand.java
@@ -5,8 +5,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class DuelCommand implements CommandExecutor {
+
+    static ArrayList<UUID> qSumo = new ArrayList<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -27,6 +34,7 @@ public class DuelCommand implements CommandExecutor {
                 sender.sendMessage(ChatUtils.color("\n &7&oYou didd technicallyyy follow the syntax...\n &7&oBut you cannot duel &e&oyourself&7, &7&oself harm is &e&oNOT the answer&7&o!\n "));
                 return true;
             }
+            // private duel code
         }
 
         return true;
