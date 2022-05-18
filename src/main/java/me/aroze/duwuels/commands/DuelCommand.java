d009aec4f93ff219edbbd7cf85eb7e0db627ca65
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-18 23:02:12 +0100

    Added syntax message

--- src/main/java/me/aroze/duwuels/commands/DuelCommand.java
+++ src/main/java/me/aroze/duwuels/commands/DuelCommand.java
@@ -1,5 +1,6 @@
 package me.aroze.duwuels.commands;
 
+import me.aroze.duwuels.util.ChatUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,7 +10,9 @@ public class DuelCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-
+        if (args.length > 1) {
+            sender.sendMessage(ChatUtils.color("&c⚠ Syntax:\n &7/duel &8[Public queues] \n &7/duel <player> &8[private duel]"));
+        }
 
         return true;
     }
