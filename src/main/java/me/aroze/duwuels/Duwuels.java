474d2dc7be92028ac3d40c29b4f32f8ac9fa6234
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-18 22:58:37 +0100

    Added/registered duel command

--- src/main/java/me/aroze/duwuels/Duwuels.java
+++ src/main/java/me/aroze/duwuels/Duwuels.java
@@ -1,5 +1,6 @@
 package me.aroze.duwuels;
 
+import me.aroze.duwuels.commands.DuelCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Duwuels extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class Duwuels extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getCommand("duel").setExecutor(new DuelCommand());
 
     }
 
