8055fa999abce2f23297579b98567463dc019448
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 01:10:54 +0100

    a

---
+++ src/main/java/me/aroze/duwuels/listeners/InventoryClick.java
@@ -0,0 +1,11 @@
+package me.aroze.duwuels.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryClick implements Listener {
+
+    public void onInventoryClick(InventoryClickEvent e) {
+    }
+
+}
