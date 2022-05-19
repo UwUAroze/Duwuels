65d175763c94c760644993c59760fec94d826833
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 21:46:30 +0100

    a

--- src/main/java/me/aroze/duwuels/listeners/InventoryClose.java
+++ src/main/java/me/aroze/duwuels/listeners/InventoryClose.java
@@ -1,11 +1,13 @@
 package me.aroze.duwuels.listeners;
 
 import me.aroze.duwuels.Duwuels;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
 public class InventoryClose implements Listener {
 
+    @EventHandler
     public void onInventoryClose(InventoryCloseEvent e) {
         if (e.getPlayer().hasMetadata("duelGui")){
             e.getPlayer().removeMetadata("duelGui", Duwuels.getInstance());
