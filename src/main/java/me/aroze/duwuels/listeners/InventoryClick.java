6f323d0e095c374c0cc124b654b4b9a0fb2ccdba
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 21:46:34 +0100

    a

--- src/main/java/me/aroze/duwuels/listeners/InventoryClick.java
+++ src/main/java/me/aroze/duwuels/listeners/InventoryClick.java
@@ -4,17 +4,21 @@ import me.aroze.duwuels.commands.DuelCommand;
 import me.aroze.duwuels.util.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class InventoryClick implements Listener {
 
+    @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
 
         if (e.getWhoClicked().hasMetadata("duelGui")) {
             DuelCommand.queueGUI((Player) e.getWhoClicked());
             e.setCancelled(true);
-            Bukkit.broadcastMessage("a");
+            if (e.getSlot() == 4) {
+                if ()
+            }
         }
 
     }
