4a17a0293252ad062cb6fa4c5a63a6a306b76c32
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 23:05:05 +0100

    a

--- src/main/java/me/aroze/duwuels/listeners/InventoryClick.java
+++ src/main/java/me/aroze/duwuels/listeners/InventoryClick.java
@@ -1,9 +1,8 @@
 package me.aroze.duwuels.listeners;
 
 import me.aroze.duwuels.commands.DuelCommand;
-import me.aroze.duwuels.handlers.DuelQueue;
+import me.aroze.duwuels.duels.SumoDuel;
 import me.aroze.duwuels.util.ChatUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,12 +17,12 @@ public class InventoryClick implements Listener {
             DuelCommand.queueGUI((Player) e.getWhoClicked());
             e.setCancelled(true);
             if (e.getSlot() == 4) {
-                if (DuelQueue.qSumo.contains(e.getWhoClicked().getUniqueId())) {
-                    DuelQueue.qSumo.remove(e.getWhoClicked().getUniqueId());
+                if (SumoDuel.queue.contains(e.getWhoClicked().getUniqueId())) {
+                    SumoDuel.queue.remove(e.getWhoClicked().getUniqueId());
                     e.getWhoClicked().sendMessage(ChatUtils.color("&7You have been &cremoved &7from the &eSumo &7queue!"));
                     return;
                 }
-                DuelQueue.qSumo.add(e.getWhoClicked().getUniqueId());
+                SumoDuel.queue.add(e.getWhoClicked().getUniqueId());
                 e.getWhoClicked().sendMessage(ChatUtils.color("&7You have been &aadded &7to the &eSumo &7queue!"));
                 // queue/game logic
                 return;
