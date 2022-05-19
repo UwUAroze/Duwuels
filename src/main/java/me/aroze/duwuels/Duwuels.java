97e9b86760085624d7c1dde13cd7be645f52db0f
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 01:46:32 +0100

    a

--- src/main/java/me/aroze/duwuels/Duwuels.java
+++ src/main/java/me/aroze/duwuels/Duwuels.java
@@ -1,6 +1,8 @@
 package me.aroze.duwuels;
 
 import me.aroze.duwuels.commands.DuelCommand;
+import me.aroze.duwuels.listeners.InventoryClick;
+import me.aroze.duwuels.listeners.InventoryClose;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Duwuels extends JavaPlugin {
@@ -9,6 +11,9 @@ public final class Duwuels extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getCommand("duel").setExecutor(new DuelCommand());
+
+        getServer().getPluginManager().registerEvents(new InventoryClick(), this);
+        getServer().getPluginManager().registerEvents(new InventoryClose(), this);
 
     }
 
