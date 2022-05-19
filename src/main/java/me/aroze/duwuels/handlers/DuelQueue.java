e95d9d8e7540d90170e20ecaf7a98c6e220af5ac
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-19 21:46:36 +0100

    a

---
+++ src/main/java/me/aroze/duwuels/handlers/DuelQueue.java
@@ -0,0 +1,10 @@
+package me.aroze.duwuels.handlers;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class DuelQueue {
+
+    public static ArrayList<UUID> qSumo = new ArrayList<>();
+
+}
