9e686d8ab89ca3e128671ff885f3f4d50f6eacec
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-20 02:14:05 +0100

    auto kill in sumo

--- src/main/java/me/aroze/duwuels/listeners/Join.java
+++ src/main/java/me/aroze/duwuels/listeners/Join.java
@@ -1,11 +1,14 @@
 package me.aroze.duwuels.listeners;
 
+import me.aroze.duwuels.Duwuels;
+import me.aroze.duwuels.util.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 public class Join implements Listener {
@@ -18,6 +21,13 @@ public class Join implements Listener {
         spawnLoc.setYaw(135);
         spawnLoc.setPitch(0);
         e.getPlayer().teleport(spawnLoc);
+
+        BukkitTask yDeathCheck = Bukkit.getScheduler().runTaskTimer(Duwuels.getInstance(), () -> {
+            double y = e.getPlayer().getLocation().getY();
+            if ( e.getPlayer().getWorld().getName().equals("Duel-Sumo-1") && y <= 64 ) {
+                e.getPlayer().setHealth(0);
+            }
+        } , 0, 1);
 
     }
 
