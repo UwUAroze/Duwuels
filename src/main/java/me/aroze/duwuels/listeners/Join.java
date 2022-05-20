2f878fdb9980660e3ac17a9f303ef183708a7765
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-20 01:50:05 +0100

    oop.

--- src/main/java/me/aroze/duwuels/listeners/Join.java
+++ src/main/java/me/aroze/duwuels/listeners/Join.java
@@ -2,11 +2,13 @@ package me.aroze.duwuels.listeners;
 
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Join implements Listener {
 
+    @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(100);
         e.getPlayer().teleport(Bukkit.getWorld("originallobby").getSpawnLocation());
