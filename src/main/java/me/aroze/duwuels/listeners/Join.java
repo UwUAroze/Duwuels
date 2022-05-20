2f582e7d0b1ad0e82c00866c7d6cf9e197a08d39
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-20 01:46:24 +0100

    Now teleports player to spawn on join

--- src/main/java/me/aroze/duwuels/listeners/Join.java
+++ src/main/java/me/aroze/duwuels/listeners/Join.java
@@ -1,5 +1,6 @@
 package me.aroze.duwuels.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +9,7 @@ public class Join implements Listener {
 
     public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(100);
+        e.getPlayer().teleport(Bukkit.getWorld("originallobby").getSpawnLocation());
     }
 
 }
