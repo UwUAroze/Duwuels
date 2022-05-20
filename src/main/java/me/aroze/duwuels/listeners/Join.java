6964e0fa84e870dffb9e321b4ddad85faa760f44
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-20 01:53:20 +0100

    (Should) point you in the right direction on spawn

--- src/main/java/me/aroze/duwuels/listeners/Join.java
+++ src/main/java/me/aroze/duwuels/listeners/Join.java
@@ -5,13 +5,14 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.util.Vector;
 
 public class Join implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(100);
-        e.getPlayer().teleport(Bukkit.getWorld("originallobby").getSpawnLocation());
+        e.getPlayer().teleport(Bukkit.getWorld("originallobby").getSpawnLocation().setDirection(new Vector(381, 9, 514)));
     }
 
 }
