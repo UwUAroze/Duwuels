318f51dbeeb6e6a45d268c16fc1adfd8b5dcb2ff
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-20 22:54:30 +0100

    Added addListener() method

--- src/main/java/me/aroze/duwuels/Duwuels.java
+++ src/main/java/me/aroze/duwuels/Duwuels.java
@@ -3,6 +3,7 @@ package me.aroze.duwuels;
 import me.aroze.duwuels.commands.DuelCommand;
 import me.aroze.duwuels.listeners.*;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Duwuels extends JavaPlugin {
@@ -31,6 +32,10 @@ public final class Duwuels extends JavaPlugin {
 
     private void addCommand(String name, CommandExecutor executor) {
         getCommand(name).setExecutor(executor);
+    }
+
+    private void addListener(Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
     }
 
 }
