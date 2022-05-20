1f7102970e9e335b054fcb5328eca49955372c57
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-20 21:47:09 +0100

    test commit

--- src/main/java/me/aroze/duwuels/listeners/Join.java
+++ src/main/java/me/aroze/duwuels/listeners/Join.java
@@ -18,6 +18,7 @@ public class Join implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         e.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(100);
 
+
         Location spawnLoc = Bukkit.getWorld("originallobby").getSpawnLocation();
         spawnLoc.setYaw(135);
         spawnLoc.setPitch(0);
