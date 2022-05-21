4654449089d0cf4e51eff9f8d71ef5ddb7d25b56
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 22:33:18 +0100

    Added test generate command for upcoming map generation

--- src/main/java/me/aroze/duwuels/commands/TestGenerate.java
+++ src/main/java/me/aroze/duwuels/commands/TestGenerate.java
@@ -1,14 +1,27 @@
 package me.aroze.duwuels.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class TestGenerate implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        Player player = (Player) sender;
+        Location middle = player.getLocation();
 
+
+        Location start = middle.clone().add(4, 4, 0);
+        for (int a = 0; a <= 8; a++) {
+            for (int b = 0; b <= 8; b++) {
+                player.getWorld().getBlockAt(start.clone().add(a, 0, b)).setType(Material.WHITE_CONCRETE);
+            }
+        }
 
         return true;
     }
