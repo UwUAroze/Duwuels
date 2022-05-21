27fab5de6db6920ec5d5409e1dc9d264631a756e
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 12:57:38 +0100

    Added leave listener

---
+++ src/main/java/me/aroze/duwuels/listeners/Leave.java
@@ -0,0 +1,12 @@
+package me.aroze.duwuels.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class Leave implements Listener {
+
+    public void onLeave(PlayerQuitEvent e) {
+        //things to do when a player leaves
+    }
+
+}
