0a9ecc58893e2ef62c4aa6780b1f03c667084293
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 13:01:16 +0100

    Leaving now removes you from the duels queue

--- src/main/java/me/aroze/duwuels/listeners/PlayerQuit.java
+++ src/main/java/me/aroze/duwuels/listeners/PlayerQuit.java
@@ -1,12 +1,13 @@
 package me.aroze.duwuels.listeners;
 
+import me.aroze.duwuels.duels.SumoDuel;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuit implements Listener {
 
     public void onLeave(PlayerQuitEvent e) {
-        //things to do when a player leaves
+        SumoDuel.queue.remove(e.getPlayer().getUniqueId());
     }
 
 }
