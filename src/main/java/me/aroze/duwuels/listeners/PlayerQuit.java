3e45eeefa006ec8edb20b7d4669e7a3d93bd608b
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 13:03:06 +0100

    oop

--- src/main/java/me/aroze/duwuels/listeners/PlayerQuit.java
+++ src/main/java/me/aroze/duwuels/listeners/PlayerQuit.java
@@ -1,11 +1,13 @@
 package me.aroze.duwuels.listeners;
 
 import me.aroze.duwuels.duels.SumoDuel;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuit implements Listener {
 
+    @EventHandler
     public void onLeave(PlayerQuitEvent e) {
         SumoDuel.queue.remove(e.getPlayer().getUniqueId());
     }
