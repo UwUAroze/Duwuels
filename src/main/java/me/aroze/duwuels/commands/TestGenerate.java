249d4a833bb56784ca03bd160c194eedfdb377a3
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 23:37:53 +0100

    Altered generation

--- src/main/java/me/aroze/duwuels/commands/TestGenerate.java
+++ src/main/java/me/aroze/duwuels/commands/TestGenerate.java
@@ -1,6 +1,8 @@
 package me.aroze.duwuels.commands;
 
+import me.aroze.duwuels.Duwuels;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -17,10 +19,12 @@ public class TestGenerate implements CommandExecutor {
 
 
         Location start = middle.clone().add(4, 0, 4);
-        for (int a = 0; a <= 8; a++) {
-            for (int b = 0; b <= 8; b++) {
-                player.getWorld().getBlockAt(start.clone().add(a, 0, b)).setType(Material.BARRIER);
-                player.getWorld().spawnFallingBlock(start.clone().add(a, 25, b), Material.PINK_CONCRETE, (byte) 0);
+
+
+        for (int x = 0; x <= 8; x++) {
+            for (int z = 0; z <= 8; z++) {
+                player.getWorld().getBlockAt(start.clone().add(x, 0, z)).setType(Material.BARRIER);
+                player.getWorld().spawnFallingBlock(start.clone().add(x, 15, z), Material.PINK_STAINED_GLASS, (byte) 0);
             }
         }
 
@@ -28,7 +32,7 @@ public class TestGenerate implements CommandExecutor {
         for (int z = 0; z <= 10; z=z+10) {
             for (int x = 0; x < 7; x++) {
                 player.getWorld().getBlockAt(start2.clone().add(x, 0, z)).setType(Material.BARRIER);
-                player.getWorld().spawnFallingBlock(start2.clone().add(x, 25, z), Material.PINK_CONCRETE, (byte) 0);
+                player.getWorld().spawnFallingBlock(start2.clone().add(x, 15, z), Material.PINK_STAINED_GLASS, (byte) 0);
             }
         }
 
@@ -36,7 +40,7 @@ public class TestGenerate implements CommandExecutor {
         for (int x = 0; x <= 10; x=x+10) {
             for (int z = 0; z < 7; z++) {
                 player.getWorld().getBlockAt(start3.clone().add(x, 0, z)).setType(Material.BARRIER);
-                player.getWorld().spawnFallingBlock(start3.clone().add(x, 25, z), Material.PINK_CONCRETE, (byte) 0);
+                player.getWorld().spawnFallingBlock(start3.clone().add(x, 15, z), Material.PINK_STAINED_GLASS, (byte) 0);
             }
         }
 
