49a36ffad5c9ca981d979dc69ffb8aec2a934c4f
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 19:41:58 +0100

    Sumo duel now stores who are playing

--- src/main/java/me/aroze/duwuels/duels/SumoDuel.java
+++ src/main/java/me/aroze/duwuels/duels/SumoDuel.java
@@ -4,18 +4,30 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class SumoDuel {
 
     public static ArrayList<UUID> queue = new ArrayList<>();
 
+    // Integer refers to arena number
+    public static HashMap<Integer, ArrayList<UUID>> playing = new HashMap<>();
+
     public static boolean start() {
         if (!(queue.size() % 2 == 0)) return false;
 
         Player Player1 = Bukkit.getPlayer(queue.get(0));
         Player Player2 = Bukkit.getPlayer(queue.get(1));
+
+        playing.put(1, new ArrayList<UUID>(){
+            {
+                add(Player1.getUniqueId());
+                add(Player2.getUniqueId());
+            }
+        } );
 
         queue.remove(Player1.getUniqueId());
         queue.remove(Player2.getUniqueId());
