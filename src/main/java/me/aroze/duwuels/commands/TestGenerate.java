7efbb87ba2474d9abf22f93d02bee9483307b35a
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 23:04:41 +0100

    Altered generation

--- src/main/java/me/aroze/duwuels/commands/TestGenerate.java
+++ src/main/java/me/aroze/duwuels/commands/TestGenerate.java
@@ -33,7 +33,7 @@ public class TestGenerate implements CommandExecutor {
         Location start3 = middle.clone().add(4, 0, 5);
         for (int x = 0; x <= 10; x=x+10) {
             for (int z = 0; z < 7; z++) {
-                player.getWorld().getBlockAt(start2.clone().add(x, 0, z)).setType(Material.ORANGE_CONCRETE);
+                player.getWorld().getBlockAt(start3.clone().add(x, 0, z)).setType(Material.LIME_CONCRETE);
             }
         }
 
