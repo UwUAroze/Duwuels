d422935d44dfdb17b8f70f56505bc39549b9dfc1
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-21 23:09:05 +0100

    Altered generation

--- src/main/java/me/aroze/duwuels/commands/TestGenerate.java
+++ src/main/java/me/aroze/duwuels/commands/TestGenerate.java
@@ -20,7 +20,7 @@ public class TestGenerate implements CommandExecutor {
         for (int a = 0; a <= 8; a++) {
             for (int b = 0; b <= 8; b++) {
                 player.getWorld().getBlockAt(start.clone().add(a, 0, b)).setType(Material.BARRIER);
-                player.getWorld().spawnFallingBlock(start.clone().add(a, 0, b), Material.PINK_CONCRETE, (byte) 0);
+                player.getWorld().spawnFallingBlock(start.clone().add(a, 10, b), Material.PINK_CONCRETE, (byte) 0);
             }
         }
 
@@ -28,7 +28,7 @@ public class TestGenerate implements CommandExecutor {
         for (int z = 0; z <= 10; z=z+10) {
             for (int x = 0; x < 7; x++) {
                 player.getWorld().getBlockAt(start2.clone().add(x, 0, z)).setType(Material.BARRIER);
-                player.getWorld().spawnFallingBlock(start2.clone().add(x, 0, z), Material.PINK_CONCRETE, (byte) 0);
+                player.getWorld().spawnFallingBlock(start2.clone().add(x, 10, z), Material.PINK_CONCRETE, (byte) 0);
             }
         }
 
@@ -36,7 +36,7 @@ public class TestGenerate implements CommandExecutor {
         for (int x = 0; x <= 10; x=x+10) {
             for (int z = 0; z < 7; z++) {
                 player.getWorld().getBlockAt(start3.clone().add(x, 0, z)).setType(Material.BARRIER);
-                player.getWorld().spawnFallingBlock(start3.clone().add(x, 0, z), Material.PINK_CONCRETE, (byte) 0);
+                player.getWorld().spawnFallingBlock(start3.clone().add(x, 10, z), Material.PINK_CONCRETE, (byte) 0);
             }
         }
 
