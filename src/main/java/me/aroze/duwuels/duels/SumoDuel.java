9a2e8e578313b7725cc0d7edcc4f4b585d0c45a9
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-22 19:30:56 +0100

    Fixed players spawning slightly off center

--- src/main/java/me/aroze/duwuels/duels/SumoDuel.java
+++ src/main/java/me/aroze/duwuels/duels/SumoDuel.java
@@ -68,11 +68,11 @@ public class SumoDuel {
 
     public static void continueSynchronously(Location arenaMiddle, Player player1, Player player2) {
 
-        Location loc1 = arenaMiddle.clone().add(5, 2, -1);
+        Location loc1 = arenaMiddle.clone().add(5, 2, 0);
         loc1.setYaw(90);
         loc1.setPitch(0);
 
-        Location loc2 = arenaMiddle.clone().add(-5, 2, 1);
+        Location loc2 = arenaMiddle.clone().add(-5, 2, 0);
         loc2.setYaw(-90);
         loc2.setPitch(0);
 
