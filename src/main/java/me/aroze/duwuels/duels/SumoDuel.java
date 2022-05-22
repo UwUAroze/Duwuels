24bcb6e34f8776b82724dc4375ad5c674d9f24b6
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-22 19:36:21 +0100

    Correctly stores what arena the player is playing on

--- src/main/java/me/aroze/duwuels/duels/SumoDuel.java
+++ src/main/java/me/aroze/duwuels/duels/SumoDuel.java
@@ -43,13 +43,6 @@ public class SumoDuel {
         queue.remove(player1.getUniqueId());
         queue.remove(player2.getUniqueId());
 
-        playing.put(1, new ArrayList<UUID>(){
-            {
-                add(player1.getUniqueId());
-                add(player2.getUniqueId());
-            }
-        } );
-
         player1.setGameMode(GameMode.SPECTATOR);
         player2.setGameMode(GameMode.SPECTATOR);
 
@@ -67,6 +60,15 @@ public class SumoDuel {
     }
 
     public static void continueSynchronously(Location arenaMiddle, Player player1, Player player2) {
+
+        int arenaNum = (arenaMiddle.getBlockX() - 500000)/500;
+        Bukkit.broadcastMessage("Playing on: Arena: #" + arenaNum);
+        playing.put(arenaNum, new ArrayList<UUID>(){
+            {
+                add(player1.getUniqueId());
+                add(player2.getUniqueId());
+            }
+        } );
 
         Location loc1 = arenaMiddle.clone().add(5, 2, 0);
         loc1.setYaw(90);
