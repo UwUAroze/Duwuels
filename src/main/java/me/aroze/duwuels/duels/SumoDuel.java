4e4e96ff5287a372fac1a905caa8085fda6e4e28
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-22 03:51:31 +0100

    Fastened duel timer

--- src/main/java/me/aroze/duwuels/duels/SumoDuel.java
+++ src/main/java/me/aroze/duwuels/duels/SumoDuel.java
@@ -64,18 +64,18 @@ public class SumoDuel {
         player2.sendTitle(ChatUtils.color("&7Found an opponent: &c" + player1), ChatUtils.color("&7The game will begin shortly. &aGood Luck!"), 5, 10, 5);
 
         Bukkit.getScheduler().runTaskLater(Duwuels.getInstance(), () -> {
-            player1.sendTitle(ChatUtils.color("&7Starting in &63..."), ChatUtils.color("&7Opponent: &c" + player2.getName()), 10, 10, 0);
-            player2.sendTitle(ChatUtils.color("&7Starting in &63..."), ChatUtils.color("&7Opponent: &c" + player1.getName()), 10, 10, 0);
+            player1.sendTitle(ChatUtils.color("&7Starting in &63..."), ChatUtils.color("&7Opponent: &c" + player2.getName()), 5, 5, 0);
+            player2.sendTitle(ChatUtils.color("&7Starting in &63..."), ChatUtils.color("&7Opponent: &c" + player1.getName()), 5, 5, 0);
         }, 20);
 
         Bukkit.getScheduler().runTaskLater(Duwuels.getInstance(), () -> {
-            player1.sendTitle(ChatUtils.color("&7Starting in &e2..."), ChatUtils.color("&7Opponent: &c" + player2.getName()), 0, 20, 0);
-            player2.sendTitle(ChatUtils.color("&7Starting in &e2..."), ChatUtils.color("&7Opponent: &c" + player1.getName()), 0, 20, 0);
+            player1.sendTitle(ChatUtils.color("&7Starting in &e2..."), ChatUtils.color("&7Opponent: &c" + player2.getName()), 0, 10, 0);
+            player2.sendTitle(ChatUtils.color("&7Starting in &e2..."), ChatUtils.color("&7Opponent: &c" + player1.getName()), 0, 10, 0);
         }, 40);
 
         Bukkit.getScheduler().runTaskLater(Duwuels.getInstance(), () -> {
-            player1.sendTitle(ChatUtils.color("&7Starting in &c1..."), ChatUtils.color("&7Opponent: &c" + player2.getName()), 0, 20, 0);
-            player2.sendTitle(ChatUtils.color("&7Starting in &c1..."), ChatUtils.color("&7Opponent: &c" + player1.getName()), 0, 20, 0);
+            player1.sendTitle(ChatUtils.color("&7Starting in &c1..."), ChatUtils.color("&7Opponent: &c" + player2.getName()), 0, 10, 0);
+            player2.sendTitle(ChatUtils.color("&7Starting in &c1..."), ChatUtils.color("&7Opponent: &c" + player1.getName()), 0, 10, 0);
         }, 60);
 
         Location start = arenaMiddle.clone().add(-4, 0, -4);
