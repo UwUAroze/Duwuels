e94be6f8365518c973dcca28df6b5f071195f82d
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-22 01:22:27 +0100

    Added back the test command for any nerds who want it

--- src/main/java/me/aroze/duwuels/commands/TestGenerate.java
+++ src/main/java/me/aroze/duwuels/commands/TestGenerate.java
@@ -19,6 +19,32 @@ public class TestGenerate implements CommandExecutor {
 
         middle.clone().add(0,-2,0).getBlock().setType(Material.BEDROCK);
 
+        Location start = middle.clone().add(-4, 0, -4);
+
+
+        for (int x = 0; x <= 8; x++) {
+            for (int z = 0; z <= 8; z++) {
+                player.getWorld().getBlockAt(start.clone().add(x, 0, z)).setType(Material.BARRIER);
+                player.getWorld().spawnFallingBlock(start.clone().add(x, 15, z), Material.PINK_STAINED_GLASS, (byte) 0);
+            }
+        }
+
+        Location start2 = middle.clone().add(-3, 0, -5);
+        for (int z = 0; z <= 10; z=z+10) {
+            for (int x = 0; x < 7; x++) {
+                player.getWorld().getBlockAt(start2.clone().add(x, 0, z)).setType(Material.BARRIER);
+                player.getWorld().spawnFallingBlock(start2.clone().add(x, 15, z), Material.PINK_STAINED_GLASS, (byte) 0);
+            }
+        }
+
+        Location start3 = middle.clone().add(-5, 0, -3);
+        for (int x = 0; x <= 10; x=x+10) {
+            for (int z = 0; z < 7; z++) {
+                player.getWorld().getBlockAt(start3.clone().add(x, 0, z)).setType(Material.BARRIER);
+                player.getWorld().spawnFallingBlock(start3.clone().add(x, 15, z), Material.PINK_STAINED_GLASS, (byte) 0);
+            }
+        }
+
 
 
 
