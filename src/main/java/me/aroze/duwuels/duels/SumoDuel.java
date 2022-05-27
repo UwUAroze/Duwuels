b82160589158c641faecfe8343411fc2187ebc05
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-27 22:12:18 +0100

    Fixed x2

--- src/main/java/me/aroze/duwuels/duels/SumoDuel.java
+++ src/main/java/me/aroze/duwuels/duels/SumoDuel.java
@@ -43,10 +43,7 @@ public class SumoDuel {
         queue.remove(player1.getUniqueId());
         queue.remove(player2.getUniqueId());
 
-        Location arenaMiddle = new Location(player1.getWorld(), 500000, 65, 500000);
-
-        player1.setGameMode(GameMode.SPECTATOR);
-        player2.setGameMode(GameMode.SPECTATOR);
+        Location arenaMiddle = new Location(player1.getWorld(), 500000, 64, 500000);
 
         Bukkit.getScheduler().runTaskAsynchronously(Duwuels.getInstance(), () -> {
             while (!(arenaMiddle.getBlock().getType().isAir())) {
@@ -77,6 +74,9 @@ public class SumoDuel {
         Location loc2 = arenaMiddle.clone().add(-5, 2, 0);
         loc2.setYaw(-90);
         loc2.setPitch(0);
+
+        player1.setGameMode(GameMode.SPECTATOR);
+        player2.setGameMode(GameMode.SPECTATOR);
 
         player1.teleport(loc1);
         player2.teleport(loc2);
