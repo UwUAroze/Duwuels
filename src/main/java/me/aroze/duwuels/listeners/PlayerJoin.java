f5cdf4e4b5932e12f95d954e5492fd0f065f1893
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-27 23:27:48 +0100

    Fixed x6..

--- src/main/java/me/aroze/duwuels/listeners/PlayerJoin.java
+++ src/main/java/me/aroze/duwuels/listeners/PlayerJoin.java
@@ -30,7 +30,7 @@ public class PlayerJoin implements Listener {
 
         BukkitTask yDeathCheck = Bukkit.getScheduler().runTaskTimer(Duwuels.getInstance(), () -> {
             double y = e.getPlayer().getLocation().getY();
-            if ( y < 65 ) {
+            if ( (!(e.getPlayer().getGameMode().equals(GameMode.SPECTATOR))) && y < 65 ) {
 
                 Player loser = null;
                 Player winner = null;
@@ -47,11 +47,6 @@ public class PlayerJoin implements Listener {
                 }
 
                 if (loser == null || winner == null) return;
-
-                if (loser.getGameMode().equals(GameMode.SPECTATOR) || winner.getGameMode().equals(GameMode.SPECTATOR)) {
-                    Bukkit.broadcastMessage("a");
-                    return;
-                }
 
                 Bukkit.broadcastMessage(y + "");
 
