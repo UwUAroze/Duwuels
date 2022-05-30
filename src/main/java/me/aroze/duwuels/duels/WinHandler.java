275593cb25f9bba6a450aaedbba1a256ccd19b3d
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-30 14:47:12 +0100

    Started win handler

---
+++ src/main/java/me/aroze/duwuels/duels/WinHandler.java
@@ -0,0 +1,17 @@
+package me.aroze.duwuels.duels;
+
+import me.aroze.duwuels.Duwuels;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
+
+public class WinHandler {
+
+
+
+    public static BukkitTask sumoCheck = Bukkit.getScheduler().runTaskTimer(Duwuels.getInstance(), () -> {
+
+        //soon:tm:
+
+    } , 0, 1);
+
+}
